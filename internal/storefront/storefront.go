86f4cdbe12279dc45438763dd842d79790f31fd9
Anthony Dip <anthonydip2001@gmail.com>
2023-09-28 21:39:06 -0400

    feat: add firebase

--- internal/storefront/storefront.go
+++ internal/storefront/storefront.go
@@ -1,9 +1,13 @@
 package storefront
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/anthonydip/flutter-messenger-go/pkg/dtos"
+
+	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go"
 )
 
 // Storefront exposes all functionalities of the Storefront service.
@@ -13,6 +17,8 @@ type Storefront interface {
 
 // Broker manages the internal state of the Storefront service.
 type Broker struct {
+	Firestore *firestore.Client
+
 	cfg Config // the storefront's configuration
 }
 
@@ -22,6 +28,19 @@ func New(cfg Config) (*Broker, error) {
 
 	if err := validateConfig(cfg); err != nil {
 		return nil, fmt.Errorf("invalid configuration: %w", err)
+	}
+
+	// Initialize firebase app
+	conf := &firebase.Config{ProjectID: "simple-messenger-53bdb"}
+	app, err := firebase.NewApp(context.Background(), conf)
+	if err != nil {
+		return nil, fmt.Errorf("error initializing firebase app: %w", err)
+	}
+
+	// Initialize firestore
+	r.Firestore, err = app.Firestore(context.Background())
+	if err != nil {
+		return nil, fmt.Errorf("error initializing firestore: %w", err)
 	}
 
 	return r, nil
