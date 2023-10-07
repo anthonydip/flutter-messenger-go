2ce2866d23ce39ed5a8fb91d3e1ca1b0e5ffaf78
Anthony Dip <anthonydip2001@gmail.com>
2023-10-07 00:15:40 -0400

    feat: handle internal routes

--- app/storefront-api/middleware/authentication.go
+++ app/storefront-api/middleware/authentication.go
@@ -2,11 +2,23 @@ package middleware
 
 import (
 	"net/http"
+	"regexp"
 
 	"github.com/anthonydip/flutter-messenger-go/app/storefront-api/webserver"
 
 	"github.com/rs/zerolog/log"
 )
+
+type route struct {
+	Regex  string
+	Method string
+}
+
+var internalRoutes = [...]route{
+	{Regex: "^/tokens/access$", Method: http.MethodGet},
+	{Regex: "^/users$", Method: http.MethodPost},
+	{Regex: "^/users/", Method: http.MethodGet},
+}
 
 // Authentication middleware
 func Authentication(srv webserver.Server) func(h http.Handler) http.Handler {
@@ -18,15 +30,23 @@ func Authentication(srv webserver.Server) func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			token := r.Header.Get("Authorization")
 
+			isInternalRoute := false
+			// Check if the request URL requires an internal token
+			for _, route := range internalRoutes {
+				match, _ := regexp.MatchString(route.Regex, r.URL.String())
+				if match {
+					isInternalRoute = true
+				}
+			}
+
 			// Perform authentication middleware depending on the route
-			switch r.URL.String() {
-			case "/tokens/access":
+			if isInternalRoute {
 				if !srv.ValidateInternalJWT(token) {
 					w.WriteHeader(http.StatusUnauthorized)
 					w.Write([]byte("401 Unauthorized"))
 					return
 				}
-			default:
+			} else {
 				if !srv.ValidateJWT(token) {
 					w.WriteHeader(http.StatusUnauthorized)
 					w.Write([]byte("401 Unauthorized"))
